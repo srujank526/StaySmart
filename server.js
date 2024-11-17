3ad16d030379ab586e3bbe8811126c2198cfae7b
Srujan <srujank526@gmail.com>
2024-11-17 14:23:04 +0530

    adding dynamic port for deployment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ dotenv.config({ path: './config.env' });
 mongoose.connect(process.env.DATABASE)
         .then(()=>console.log("connected to DB"))
 
-app.listen(8000,()=>{
+const PORT  = process.env.PORT || 8000
+app.listen(PORT,()=>{
     console.log("server is listening on port 8000")
 })
